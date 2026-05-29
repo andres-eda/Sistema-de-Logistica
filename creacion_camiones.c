a9bfe6a80c51b5c455f6867b709437fd776829a3
andres-eda <andresivan.rivasmiranda@gmail.com>
2026-05-28 22:35:15 -0600

    Update creacion_camiones.c

--- creacion_camiones.c
+++ creacion_camiones.c
@@ -2,13 +2,11 @@ void creacion_camiones()
 {
     //Declaracion de variables 
     int i;
-    int kg=1;  
-    int suma=/1;
     //Algoritmo 
     for(i=0; i<5; i++)
     {
-        Camion transporte = {suma, kg*100, 0.0 };
-        suma++;
-        insertarCamion(&listaCamiones, transporte);
+        Camion transporte = {i+1, (i+1)*100, 0.0};
+        insertarCamion(&listaCamiones, transporte); 
     }
 }
+        
