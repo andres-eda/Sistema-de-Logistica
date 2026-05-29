e7c2f671f4142f40c01302c22d3444207394dd5a
MendozaReyesVictoriaCamila <vica.menrey@gmail.com>
2026-05-28 23:54:22 -0600

    Update historial_vacio.c

--- historial_vacio.c
+++ historial_vacio.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "estructuras.h"
 int historial_vacio(Historial *h) {
     //retorna verdadedo solo si el inicio del historil es null 
     return (h->cabeza == NULL);
