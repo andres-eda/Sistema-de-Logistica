92d8b68f2ca20ed9a618c71a158c6c0da0f8df98
andres-eda <andresivan.rivasmiranda@gmail.com>
2026-05-28 19:12:37 -0600

    Add files via upload

---
+++ registrar_entrega.c
@@ -0,0 +1,25 @@
+int registrar_entrega(Historial *h, Paquete paquete, int id_camion) {
+    NodoHistorial *nuevo = (NodoHistorial *)malloc(sizeof(NodoHistorial));
+    if (nuevo == NULL) {
+        return 0;  /*Aqui es para error de memoria */
+    }
+
+    nuevo->entrega.paquete = paquete;
+    nuevo->entrega.id_camion = id_camion;
+    nuevo->siguiente = NULL;
+
+    if (h->cabeza == NULL) {
+        /* En la lista vacia el nuevo es el primero */
+        h->cabeza = nuevo;
+    } else {
+        /* Y aca es para buscar el ultimo nodo y enganchar */
+        NodoHistorial *actual = h->cabeza;
+        while (actual->siguiente != NULL) {
+            actual = actual->siguiente;
+        }
+        actual->siguiente = nuevo;
+    }
+
+    h->total_entregas++;
+    return 1;
+}
