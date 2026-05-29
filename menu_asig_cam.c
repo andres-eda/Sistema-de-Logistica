a87341714a03af29a913ad22c805fecced239f2a
andres-eda <andresivan.rivasmiranda@gmail.com>
2026-05-28 18:28:32 -0600

    Update menu_asig_cam.c

--- menu_asig_cam.c
+++ menu_asig_cam.c
@@ -2,15 +2,13 @@ void menu_asig_cam() {
     //Declaración de variables
     NodoCamion* listaCamiones = NULL;
     int opcion = 0;
-
     //Algoritmo 
-    Camion c1 = {1, 500.0, 0.0};
-    Camion c2 = {2, 300.0, 0.0};
-    insertarCamion(&listaCamiones, c1);
-    insertarCamion(&listaCamiones, c2);
-
+    if(listaCamiones==NULL)
+        {
+            creacion_camiones(); 
+        }
     do {
-        printf("\n=== SISTEMA DE AAIGNACION DE PAQUETES ===\n");
+        printf("\n=== SISTEMA DE ASIGNACION DE PAQUETES ===\n");
         printf("1. Rotar turno de camiones\n");
         printf("2. Asignar paquete\n");
         printf("3. Salir\n");
