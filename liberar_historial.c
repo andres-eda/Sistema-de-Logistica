02942f54da668ce84a785b14d223f82a22313b5d
MendozaReyesVictoriaCamila <vica.menrey@gmail.com>
2026-05-28 23:03:41 -0600

    dokumentado

--- liberar_historial.c
+++ liberar_historial.c
@@ -1,11 +1,17 @@
 void liberar_historial(Historial *h) {
+    //punteros auxiliares para recorrer la lista sin perdeer la referencia 
     NodoHistorial *actual = h->cabeza;
     NodoHistorial *siguiente;
+    //recorre todos los nodos de la lista hasta llegar al final 
     while (actual != NULL) {
+        //guarda el siguiente nodo antes de borrar el actual 
         siguiente = actual->siguiente;
+        //libera la memoria RAM del nodo actual
         free(actual);
+        //avanza al siguiente nodo para continuar el ciclo 
         actual = siguiente;
     }
+    //reincia los valores del historial para dejarlos vacio y seguro 
     h->cabeza = NULL;
     h->total_entregas = 0;
 }
