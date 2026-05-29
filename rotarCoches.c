1c91b29a26173f80d147a410e8b4ac0af2cd1f7e
MendozaReyesVictoriaCamila <vica.menrey@gmail.com>
2026-05-28 23:50:22 -0600

    Update rotarCoches.c

--- rotarCoches.c
+++ rotarCoches.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "estructuras.h"
 void rotarCoches(NodoCamion** cabeza) {
     if (*cabeza != NULL) {
         *cabeza = (*cabeza)->siguiente; 
