b150cfd7af48151469e1809bc2aad8999579ce02
MendozaReyesVictoriaCamila <vica.menrey@gmail.com>
2026-05-28 22:56:24 -0600

    dokumentado

--- inicializar_historial.c
+++ inicializar_historial.c
@@ -1,4 +1,6 @@
 void inicializar_historial(Historial *h) {
+    //establece el inicio de la lista como nulo 
     h->cabeza = NULL;
+    //inicia el contador total de entregas registradas en cero 
     h->total_entregas = 0;
 }
