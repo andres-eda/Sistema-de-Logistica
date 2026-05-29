4fcd9ab42e3a9f68514cf1f3c6276b785f83c031
MendozaReyesVictoriaCamila <vica.menrey@gmail.com>
2026-05-29 02:34:33 -0600

    Update imprimir_historial.c

--- imprimir_historial.c
+++ imprimir_historial.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "estructuras.h"
 void imprimir_historial(Historial *h) {
     if (historial_vacio(h)) {
         printf("El historial esta vacio. No hay registros.\n");
