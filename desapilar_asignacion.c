445d5a0f3601bfc1dccd9c924b4fcd23884b1457
MendozaReyesVictoriaCamila <vica.menrey@gmail.com>
2026-05-28 23:54:45 -0600

    Update desapilar_asignacion.c

--- desapilar_asignacion.c
+++ desapilar_asignacion.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "estructuras.h"
 int desapilar_asignacion(PilaDeshacer *p, Asignacion *salida) {
     if (pila_vacia(p)) {
         return 0;
