fc5dc738a18713944851c726b15b7c7473fd83c7
MendozaReyesVictoriaCamila <vica.menrey@gmail.com>
2026-05-28 22:57:36 -0600

    dokumentado

--- inicializar_pila.c
+++ inicializar_pila.c
@@ -1,4 +1,6 @@
 void inicializar_pila(PilaDeshacer *p) {
+    //establece la cima de la pila como nula 
     p->tope = NULL;
+    //inicia el contador total de eleentos en cero 
     p->total = 0;
 }
