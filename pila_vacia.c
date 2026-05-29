83e331bf39a3f88d9638a4cd1de09f0c7e9a04b6
MendozaReyesVictoriaCamila <vica.menrey@gmail.com>
2026-05-28 23:51:21 -0600

    Update pila_vacia.c

--- pila_vacia.c
+++ pila_vacia.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "estructuras.h"
 int pila_vacia(PilaDeshacer *p) {
     //retorna si el tope es NULL indicando que no hay elementos
     return (p->tope == NULL);
