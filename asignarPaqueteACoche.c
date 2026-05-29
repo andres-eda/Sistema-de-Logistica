452b38ebcf799c4a3f2e9575f4a21f999c9df3bb
MendozaReyesVictoriaCamila <vica.menrey@gmail.com>
2026-05-28 22:34:06 -0600

    dokumentado

--- asignarPaqueteACoche.c
+++ asignarPaqueteACoche.c
@@ -1,12 +1,18 @@
 void asignarPaqueteACoche(void* cola, NodoCamion** camionActual, void* topePila) {
+    //validacion de seguridad verifica si la estructura de la cola existe en memoria
     if (cola == NULL) {
+        //muestra una alerta indicando que no se ha creado la cola de espera 
         printf("Alerta: Estructura de cola no inicializada.\n");
         return;
     }
+    //validacion de seguridad comprueba que la lista circular tenga al menus un camion 
     if (*camionActual == NULL) {
+        //muestra un eeror si el usuario intenta asignar carga sin tener flota displonible 
         printf("Error: No se han registrado camiones en el sistema.\n");
-        return;
+        return; //termina la ejecucion d ela funcion anticipadamente 
     }
+    //mensaje de trazabilidad confirma en cnsola que las validaciones pasaron 
+    //el modulo esta listo para interactuar con el menu
 
     printf("[LOGÍSTICA] Módulo listo. Conectando asignación con el menú principal...\n");
 }
