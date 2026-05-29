a002a80be1095b2652718ede063515ff8c694dbf
MendozaReyesVictoriaCamila <vica.menrey@gmail.com>
2026-05-28 22:30:12 -0600

    dokumentado

--- apilar_asignacion.c
+++ apilar_asignacion.c
@@ -1,14 +1,22 @@
 int apilar_asignacion(PilaDeshacer *p, Paquete paquete, int id_camion) {
+//reserva de memoria dinamica para el nuevo nodo de la pila
     NodoPila *nuevo = (NodoPila *)malloc(sizeof(NodoPila));
     if (nuevo == NULL) {
+        //imprime un mensade de eeror si el sistema se quedo sin memoria RAM
         puts("ERROR 1. Error en memoria dinamica");
-        return 0;
+        return 0;//retorna 0 para indicar que fallo la operacion
     }
+    //copia los datos del paquete al nuevo nodo que se acaba de crear 
 
     nuevo->asignacion.paquete = paquete;
+    //duarda el ID del camion en el nuevo nodo para tener el registro completo
     nuevo->asignacion.id_camion = id_camion;
+    //coneta el nuevo nodo con el resto de la pila
     nuevo->siguiente = p->tope;  /* el nuevo apuntaría al tope actual */
+    //actuaiza el indicador de la cima de la pila para que sea este nuevo nodo 
     p->tope = nuevo;             /* entonces el nuevo es ahora el tope */
+    //incrementa el contador total de elementos guardados en la pila
     p->total++;
+    //retorna 1 indicando que el guardado en el historial fue exitoso
     return 1;
 }
