ee6948705633423070c25f739ee2346bc2a6158a
MendozaReyesVictoriaCamila <vica.menrey@gmail.com>
2026-05-28 23:53:44 -0600

    Update inicializar_pila.c

--- inicializar_pila.c
+++ inicializar_pila.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "estructuras.h"
 void inicializar_pila(PilaDeshacer *p) {
     //establece la cima de la pila como nula 
     p->tope = NULL;
